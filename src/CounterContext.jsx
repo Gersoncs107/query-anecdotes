71839b1c14fd9564247cf4dc7b11055228eaf1d8
Gersoncs107 <gerson.silva107@gmail.com>
2026-02-26 22:07:18 -0300

    query-anecdotes-update

--- src/CounterContext.jsx
+++ src/CounterContext.jsx
@@ -2,4 +2,15 @@ import { createContext, useReducer, useContext } from 'react'
 
 const CounterContext = createContext()
 
+const nottificationReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET':
+      return action.payload
+    case 'CLEAR':
+      return null
+    default:
+      return state
+  }
+}
+
 export default CounterContext
