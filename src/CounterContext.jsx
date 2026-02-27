85261ba7915ec005a20b9673b792b45819b709de
Gersoncs107 <gerson.silva107@gmail.com>
2026-02-26 22:20:20 -0300

    query-anecdotes-update

--- src/CounterContext.jsx
+++ src/CounterContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useReducer, useContext } from 'react'
 
 const CounterContext = createContext()
 
-const nottificationReducer = (state, action) => {
+const notificationReducer = (state, action) => {
   switch (action.type) {
     case 'SET':
       return action.payload
@@ -13,14 +13,27 @@ const nottificationReducer = (state, action) => {
   }
 }
 
-export const CounterContextProvider = (children) => {
-    const [notification, dispatch] = useReducer(nottificationReducer, null)
+export const CounterContextProvider = ({ children }) => {
+  const [notification, dispatch] = useReducer(notificationReducer, null)
 
-    return (
-      <CounterContext.Provider value={[notification, dispatch]}>
-        {children}
-      </CounterContext.Provider>
-    )
+  return (
+    <CounterContext.Provider value={[notification, dispatch]}>
+      {children}
+    </CounterContext.Provider>
+  )
+}
+
+export const useNotification = () => {
+  const [, dispatch] = useContext(CounterContext)
+
+  const setNotification = (message) => {
+    dispatch({ type: 'SET', payload: message })
+    setTimeout(() => {
+      dispatch({ type: 'CLEAR' })
+    }, 5000)
+  }
+
+  return setNotification
 }
 
 export default CounterContext
